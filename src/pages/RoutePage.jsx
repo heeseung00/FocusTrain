109d9b0a8b4ccddef799064cebfa99dbd09e19fa
heeseung <love01040701320@gmail.com>
2026-08-06 00:10:12 +0900

    [오늘 배운 것]
    1. json 데이터를 가져와서 사용하는 법
    -> json을 import하고 json이름( ex) stationList )을 가져와서 사용하면 안에 값 ( ex)  id, city, times, times.ktx )을 꺼내쓸 수있다.
    
    2.  json데이터를 조건부로 랜더링 하고싶을 때 작성방법
    -> 내 경우에 기차를 선택하는 버튼을 누르면 select의 option에 들어가는 city의 이름이 times안에 각 값이 있는지 또는 null인지의 여부에 따라 이름을 제외하여 그에 따른 option을 만들고자 했다.
    이때 filter를 사용하면 특정 조건 랜더링이 가능하다.
    그리고 특정조건에 따라 목록을 먼저 구분하고, 이 구분된 목록을 select에 가져와 map으로 반복해서 만들면된다.
    애초에 나는 map에서 특정조건랜더링을 해야한다고 생각해 어려움이 있었는데
    조건에 따른 목록이 담긴 함수를 먼저 만들어야했던거다.

--- src/pages/RoutePage.jsx
+++ src/pages/RoutePage.jsx
@@ -10,6 +10,24 @@ function RoutePage() {
         setSelected(e.target.value);
     };
 
+    const filterStation = stationList.filter((item) => {
+        if (train === 'KTX') {
+            return item.times.ktx !== null;
+        }
+        if (train === 'ITX') {
+            return item.times.ITX !== null;
+        }
+        if (train === '무궁화') {
+            return item.times.mugunghwa !== null;
+        }
+    });
+    // 기차 종류를 선택했을때, 그에 따라 selectbox의 option에 다른 값을 받아와야한다.
+    // 근데? 아니지. 아 맞지 option의 내용이 달라지는거니까.
+    // 현재는 map item으로 json데이터를 연결해 city의 값으로 받아오고 있으므로,
+    // 조건에 times를 걸어서 만약에 onClick된 setTrain값에 따라 -
+    // times의 값이 있다면 해당 데이터를 받아오고, 아닐 때는 그 값을 안 받아온다!
+    // 라고 조건을 짠다면.. 선택값에 따른 option 값을 다르게 받아올 수 있을 것 같은데?
+
     return (
         <section id="center">
             <form onSubmit="handleSubmit">
@@ -64,11 +82,11 @@ function RoutePage() {
                                 </option>
                             </select>
                         </div>
-                        <div>⇔</div>
+                        <button>⇔</button>
                         <div>
                             <h4>도착</h4>
                             <select onChange={handleSelect} value={selected}>
-                                {stationList.map((item) => {
+                                {filterStation.map((item) => {
                                     return (
                                         <option value={item.city} key={item.id}>
                                             {item.city}
