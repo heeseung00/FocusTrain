7ea725b88f31cf9451d0ab7c1a8f7526a795d974
heeseung <love01040701320@gmail.com>
2026-08-11 16:50:55 +0900

    공통 사용state를 context 파일에 분리

---
+++ src/context/TripContext.jsx
@@ -0,0 +1,15 @@
+function TripContext() {
+    //  ---- 선택 상태 ----
+    const [train, setTrain] = useState('KTX');
+    // 역(도착지) 선택
+    const [selected, setSelected] = useState('선택');
+    // 토글 버튼
+    const [isToggleOn, setIsToggleOn] = useState(true);
+    // 휴식시간 조절할 수 있게 (중간 정차역)
+    const [focusTime, setFocusTime] = useState(0);
+    // return(
+
+    // )
+}
+
+export default TripContext;
