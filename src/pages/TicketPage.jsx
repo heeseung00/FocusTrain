444770147791e177478a7672447e8848898d3642
heeseung <love01040701320@gmail.com>
2026-08-12 23:41:21 +0900

    1. TicketPage에 출발지 시간에 소요시간을 더해 도착지 시간을 출력하는 걸 추가했다.
    2. 그리고 TicketPage에 기존에 티켓에 마우스를 hover하면 티켓이 찢어지는 애니메이션이 작동했는데, 티켓이 찢어지면 다음페이지로 이동하기 위해, 마우스 클릭시 애니메이션+페이지 이동이 되도록 수정했다.
    
    3. 페이지 이동을 확인하기 위해 TimerPage.jsx에 임시 데이터를 추가했다.

--- src/pages/TicketPage.jsx
+++ src/pages/TicketPage.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/TicketPage.css';
 import { stationList } from '../utils/stationList.js';
 import { formatTime } from '../pages/RoutePage.jsx';
@@ -22,26 +24,68 @@ import { useTrip } from '../context/TripContext.jsx';
 // └────────────────────────────────────┘
 
 function TicketPage(row, seatType) {
-    // ---- 선택 상태 ----
-    // const { train, setTrain, selected, setSelected, isToggleOn, setIsToggleOn, focusTime, setFocusTime } = useTrip();
+    const navigate = useNavigate();
+    const [isOpened, setIsOpened] = useState(false);
 
-    // 오늘 날짜 표시
-    function TodayDate() {
+    function getToday() {
         const today = new Date();
-        const year = today.getFullYear();
-        const month = today.getMonth() + 1;
-        const date = today.getDate();
 
         const days = ['일', '월', '화', '수', '목', '금', '토'];
-        const dayName = days[today.getDay()];
 
+        return {
+            year: today.getFullYear(),
+            month: today.getMonth() + 1,
+            date: today.getDate(),
+            day: days[today.getDay()],
+            hours: today.getHours(),
+            min: today.getMinutes(),
+        };
+    }
+
+    // 반복되므로 TicketPage안에서 전역사용
+    const today = getToday();
+
+    // 오늘 날짜 표시- 년, 월, 일, 요일
+    function TodayDate() {
         return (
-            <div>
-                {year}년 {month}월 {date}일 ({dayName})
-            </div>
+            // p태그 안에는 div 넣기 불가. 만들 때 확인
+            <>
+                {today.year}년 {today.month}월 {today.date}일 ({today.day})
+            </>
         );
     }
 
+    // 현재시간 표시 - 시, 분
+    function TodayTime() {
+        return (
+            <>
+                {String(today.hours).padStart(2, '0')}:{String(today.min).padStart(2, '0')}
+            </>
+        );
+    }
+
+    // 시간 계산 - 출발지 시간 + 소요시간 = 도착지 시간
+    function tripTime() {
+        function getArriveTime() {
+            // 시간을 분단위로 바꾼다 - ex) 16시간 = 60분 = 960분
+            const total = today.hours * 60 + today.min + travelTime;
+            // Math.floor로 소수점을 버리고 그 숫자에 24를 나누어 24시간이 넘어가면 다시 0부터 시작하도록 한다.
+            const hours = String(Math.floor(total / 60) % 24).padStart(2, '0');
+            const min = String(total % 60).padStart(2, '0');
+
+            return `${hours}:${min}`;
+        }
+
+        return getArriveTime();
+    }
+
+    function handleTicketClick() {
+        setIsOpened(true);
+
+        setTimeout(() => {
+            navigate('/timer');
+        }, 800);
+    }
     // 열차 선택값 받아오기
     const { train, selected, activeCoach, setActiveCoach, selectedSeat, setSelectedSeat } = useTrip();
 
@@ -67,7 +111,7 @@ function TicketPage(row, seatType) {
             <div className="ticket-page">
                 <div className="ambient-light"></div>
 
-                <div className="ticket-container" id="ticket">
+                <div className={`ticket-container ${isOpened ? 'opened' : ''}`} id="ticket" onClick={handleTicketClick}>
                     <article className="ticket-main">
                         <div className="ticket-content">
                             <header className="ticket-header">
@@ -79,12 +123,12 @@ function TicketPage(row, seatType) {
                                 <ul className="ticket-title">
                                     <li className="depart">
                                         <h1 className="title">서울</h1>
-                                        <p className="time">15:08</p>
+                                        <p className="time">{TodayTime()}</p>
                                     </li>
                                     <li className="arrow">→</li>
                                     <li className="arrive">
                                         <h1 className="title">{selected}</h1>
-                                        <p className="time">16:13</p>
+                                        <p className="time">{tripTime()}</p>
                                     </li>
                                 </ul>
                                 <div className="Destination"></div>
---
+++ src/pages/TimerPage.jsx
@@ -0,0 +1,9 @@
+function TimerPage() {
+    return (
+        <section id="center">
+            <h1>타이머</h1>
+        </section>
+    );
+}
+
+export default TimerPage;
