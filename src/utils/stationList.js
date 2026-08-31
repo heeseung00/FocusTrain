69041f2427921cf1c85d5f523aff0daf0e9227b8
heeseung <love01040701320@gmail.com>
2026-08-31 14:47:00 +0900

    1. 가독성 높은 코드로 리팩토링. 2. 휴식(중간정차)을 토글햇을 때, 시간 횟수 (20분마다 1회) 안내 글자를 보이게하고, 기차 시간 만큼 휴식 시간의 갯수도 달라지게 한다. 3. 시간 조정 - TimeControl 을 추가했다. 현재 데이터는 실제 기차 시간을 반영하여 최대 집중 시간이 5시간 30분인데, 실사용자는 더 길게 집중하고싶을 수 잇으므로 스스로 시간을 조정할 수 있는 부분을 만든다. 4. stationList.js(data json) 소요시간이 기존에 '' 문자열로 잘못입력되어있어, 시간을 조정했을때 숫자가 아닌 문자열로 인식하여 오류가 생기므로, 숫자형태로 변경했다. 5. 버튼을 누르면 다음페이지인 티켓페이지로 이동하게 만들었다. -라우터를 install하여 app.jsx 형식을 라우터 형식으로 바꾸고 메인과 티켓 페이지를 app.jsx에 작성한다. 그리고 메인페이지에서 navigate를 사용해 라우터로 작성해두었던 티켓페이지로 이동할 수 있게 했다.
    -----------------------
    
    수정이 필요한 점
    생각해보니..사용자가 시간을 변경했을때 휴식시간 갯수도 달라져야할 것 같다.

--- src/utils/stationList.js
+++ src/utils/stationList.js
@@ -5,9 +5,9 @@ export const stationList = [
         line: '경부선',
         times: {
             // 분단위로 입력 후 나중에 ${hour}시간 ${minute}분`으로 출력
-            ktx: '25',
-            itx: '30',
-            mugunghwa: '35',
+            ktx: 25,
+            itx: 30,
+            mugunghwa: 35,
         },
         distance: '41km',
     },
@@ -16,9 +16,9 @@ export const stationList = [
         city: '부산',
         line: '경부선',
         times: {
-            ktx: '150',
-            itx: '280',
-            mugunghwa: '330',
+            ktx: 150,
+            itx: 280,
+            mugunghwa: 330,
         },
         distance: '423.9km',
     },
@@ -27,9 +27,9 @@ export const stationList = [
         city: '대구(동대구)',
         line: '경부선',
         times: {
-            ktx: '110',
-            itx: '210',
-            mugunghwa: '240',
+            ktx: 110,
+            itx: 210,
+            mugunghwa: 240,
         },
         distance: '287.9km',
     },
@@ -38,9 +38,9 @@ export const stationList = [
         city: '대전',
         line: '경부선',
         times: {
-            ktx: '60',
-            itx: '110',
-            mugunghwa: '120',
+            ktx: 60,
+            itx: 110,
+            mugunghwa: 120,
         },
         distance: '159.8km',
     },
@@ -49,7 +49,7 @@ export const stationList = [
         city: '포항',
         line: '경부선(포항 KTX)',
         times: {
-            ktx: '130',
+            ktx: 130,
             itx: null,
             mugunghwa: null,
         },
@@ -60,7 +60,7 @@ export const stationList = [
         city: '광주(광주송정역)',
         line: '호남선',
         times: {
-            ktx: '90',
+            ktx: 90,
             itx: null,
             mugunghwa: null,
         },
@@ -82,7 +82,7 @@ export const stationList = [
         city: '여수(여수엑스포)',
         line: '전라선',
         times: {
-            ktx: '180',
+            ktx: 180,
             itx: null,
             mugunghwa: null,
         },
@@ -93,7 +93,7 @@ export const stationList = [
         city: '전주',
         line: '전라선',
         times: {
-            ktx: '100',
+            ktx: 100,
             itx: null,
             mugunghwa: null,
         },
@@ -104,7 +104,7 @@ export const stationList = [
         city: '강릉',
         line: '경강선',
         times: {
-            ktx: '120',
+            ktx: 120,
             itx: null,
             mugunghwa: null,
         },
@@ -115,7 +115,7 @@ export const stationList = [
         city: '원주',
         line: '경강선',
         times: {
-            ktx: '50',
+            ktx: 50,
             itx: null,
             mugunghwa: null,
         },
@@ -126,7 +126,7 @@ export const stationList = [
         city: '천안아산',
         line: '경부선',
         times: {
-            ktx: '30',
+            ktx: 30,
             itx: null,
             mugunghwa: null,
         },
@@ -137,7 +137,7 @@ export const stationList = [
         city: '익산',
         line: '호남선',
         times: {
-            ktx: '90',
+            ktx: 90,
             itx: null,
             mugunghwa: null,
         },
@@ -148,7 +148,7 @@ export const stationList = [
         city: '순천',
         line: '전라선',
         times: {
-            ktx: '160',
+            ktx: 160,
             itx: null,
             mugunghwa: null,
         },
@@ -159,8 +159,8 @@ export const stationList = [
         city: '창원',
         line: '경전선',
         times: {
-            ktx: '180',
-            itx: '300',
+            ktx: 180,
+            itx: 300,
             mugunghwa: null,
         },
         distance: '400km',
